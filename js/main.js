e596fe1c44e8b6f768067f6e48d9625f0e96103b
imshubhamkumar <shubhamk773@gmail.com>
2020-09-21 02:27:34 +0530

    meme structure changes

--- js/main.js
+++ js/main.js
@@ -114,7 +114,7 @@
   // Testimonials carousel (uses the Owl Carousel library)
   $(".testimonials-carousel").owlCarousel({
     autoplay: true,
-    dots: true,
+    //dots: true,
     loop: true,
     items: 1
   });
